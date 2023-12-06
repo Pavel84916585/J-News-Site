3d93395fcc31a92e9fe75a55c7757f903d9641cd
AiratMyltykbaev <airat7300@gmail.com>
2023-12-06 23:51:49 +0300

    first fitcha po besopasnosti kompa(serva)

--- src/main/java/RBPO/RBPO/services/AppUserService.java
+++ src/main/java/RBPO/RBPO/services/AppUserService.java
@@ -16,24 +16,48 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+
 public class AppUserService {
+    boolean TestPassword (String psw) {
+        if(psw.length()>30)
+            return false;
+        int balls = 0;
+        balls += psw.length() >= 8? 10: 0;  // более 8 знаков
+        balls += psw.chars()                // 2 буквы
+                .filter(i -> "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".contains("" + (char) i)).count() > 1? 10: 0;
+        balls += psw.chars()                // 2 цифры
+                .filter(i -> "0123456789".contains("" + (char) i)).count() > 1? 10: 0;
+        balls += psw.chars()                // одна или больше больших букв
+                .anyMatch(i -> "ABCDEFGHIJKLMNOPQRSTUVWXYZ".contains("" + (char) i))? 10: 0;
+        balls += psw.chars()                // одна или больше маленьких букв
+                .anyMatch(i -> "abcdefghijklmnopqrstuvwxyz".contains("" + (char) i))? 10: 0;
+        balls += psw.chars()                // содержатся спецсимволы (дополнить список символов по желанию)
+                .anyMatch(i -> "!@#$%^&*[]".contains("" + (char) i))? 10: 0;
+        if (balls >=50)
+            return true;
+
+        return false;
+    }
+
+
     private final AppUserRepository appUserRepository;
 
     //хэширует пароль пользователя
     @Autowired
     private BCryptPasswordEncoder encoder(){return new BCryptPasswordEncoder();}
 
-   /* public List<AppUser> listAppUser(String email) {
-        if (email != null) return appUserRepository.findByEmail(email);
-        return appUserRepository.findAll();
-    }*/
+    /* public List<AppUser> listAppUser(String email) {
+         if (email != null) return appUserRepository.findByEmail(email);
+         return appUserRepository.findAll();
+     }*/
     public void saveAppUser(AppUser appUser) {
-        //берем введенный пароль и хэшируем его, перезаписывая вместо "чистого" пароля
-        appUser.setPasswordHash(encoder().encode(appUser.getPasswordHash()));
-
-        log.info("Saving new {}", appUser);
-        //сохраняем юхера в бд
-        appUserRepository.save(appUser);
+        if (TestPassword(appUser.getPasswordHash())) {
+            //берем введенный пароль и хэшируем его, перезаписывая вместо "чистого" пароля
+            appUser.setPasswordHash(encoder().encode(appUser.getPasswordHash()));
+            log.info("Saving new {}", appUser);
+            appUserRepository.save(appUser);
+            //сохраняем юзера в бд
+        }
     }
     public void deleteAppUser(Long id) {
         appUserRepository.deleteById(id);
@@ -42,16 +66,5 @@ public class AppUserService {
         return appUserRepository.findById(id).orElse(null);
     }
 
-
-    /*public AppUser findByEmail(String email)
-    {
-
-        return
-    }*/
-
-    /*
-    *
-    *
-    */
 
 }
